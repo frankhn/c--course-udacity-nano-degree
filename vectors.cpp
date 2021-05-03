6511a13176ea782e6bafce1f1664115b1ed38560
frankhn <harfrank2@gmail.com>
2021-05-03 08:29:11 +0200

    vectors

--- vectors.cpp
+++ vectors.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+
 using std::cout;
+using std::ifstream;
+using std::istringstream;
+using std::string;
 using std::vector;
+
+/*
+Write a vector<int> ParseLine function which 
+accepts a string as an argument. 
+Use std::istringstream to parse the line by
+ comma chars, and store the ints in a 
+ vector<int>. ParseLine should return the vector
+  after parsing.
+
+*/
+vector<int> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<int> row;
+    while (sline >> n >> c && c == ',') {
+      row.push_back(n);
+    }
+    return row;
+}
+
+vector<vector<int> > ReadBoardFile(string path) {
+  ifstream myfile (path);
+  vector<vector<int> > board{};
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      vector<int> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
+}
+
+#include "test.cpp"
 
 int main() {
     // there ways if declaring and initializing vectors
@@ -39,4 +81,38 @@ int main() {
                              {3, 1, 2, 3}};
 
     cout << b[0].size() << "\n";
+
+
+    // adding content to a vector
+    vector<int> v {1, 2, 3};
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << "\n";
+    }
+
+    // push 4 to the back of the vector
+    v.push_back(4);
+
+    // print the content again
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << "print vector values again" << v[i] << "\n";
+    }
+
+  TestParseLine();
+
+//   Formatting a printed board
+// Create the enum Color with fixed values.
+    enum class Color {white, black, blue, red};
+
+    // Create a Color variable and set it to Color::blue.
+    Color my_color = Color::blue;
+
+    // Test to see if my car is red.
+    if (my_color == Color::red) {
+        cout << "The color of my car is red!" << "\n";
+    } else {
+        cout << "The color of my car is not red." << "\n";
+    }
 }
